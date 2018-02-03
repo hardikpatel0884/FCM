83a70dba2d46b710374883176c8fadd3c42b433d
Hardik Patel <hardikpatel0884@gmail.com>
2018-02-03 17:08:55 +0530

    customize notification like instagram

--- app/src/main/java/com/test/fcm/MainActivity.java
+++ app/src/main/java/com/test/fcm/MainActivity.java
@@ -1,15 +1,26 @@
 package com.test.fcm;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,11 +28,15 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.test.fcm.config.Config;
 import com.test.fcm.utils.NotificationUtils;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private BroadcastReceiver mRegistrationBroadcastReceiver;
-    private TextView txtRegId, txtMessage;
+    private TextView txtRegId, txtMessage,tvTitle;
+    private NotificationUtils notificationUtils;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
 
         txtRegId = findViewById(R.id.txt_reg_id);
         txtMessage = findViewById(R.id.txt_push_message);
+        toolbar = findViewById(R.id.layout_top);
+        setSupportActionBar(toolbar);
+        tvTitle=findViewById(R.id.tv_notif_title);
 
         mRegistrationBroadcastReceiver = new BroadcastReceiver() {
             @Override
@@ -47,10 +65,78 @@ public class MainActivity extends AppCompatActivity {
                     // new push notification is received
 
                     String message = intent.getStringExtra("message");
-
-                    Toast.makeText(getApplicationContext(), "Push notification: " + message, Toast.LENGTH_LONG).show();
-
                     txtMessage.setText(message);
+
+
+                    // top notification
+                    /*NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this);
+                    builder.setSmallIcon(R.mipmap.ic_launcher_round)
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground))
+                            .setColor(getResources().getColor(R.color.colorPrimary))
+                            .setContentTitle(message)
+                            .setContentText(strTitle)
+                            .setDefaults(Notification.DEFAULT_ALL)
+                            .setPriority(NotificationManager.IMPORTANCE_HIGH);
+
+                    builder.setAutoCancel(false);
+                    NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                    mNotificationManager.notify(0, builder.build());*/
+
+                    final LinearLayout ll=findViewById(R.id.notif);
+                    tvTitle.setText(message);
+                    ll.setVisibility(View.VISIBLE);
+                    Animation animation= AnimationUtils.loadAnimation(MainActivity.this,R.anim.top_sheet_slide_in);
+                    animation.setDuration(500);
+                    ll.setAnimation(animation);
+                    ll.animate();
+                    animation.start();
+
+                    final Animation animationout= AnimationUtils.loadAnimation(MainActivity.this,R.anim.top_sheet_slide_out);
+                    animationout.setDuration(500);
+                    animationout.setStartOffset(5000);
+
+                    animation.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            ll.setAnimation(animationout);
+                            ll.animate();
+                            animation.start();
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+
+                        }
+                    });
+
+
+
+
+                    animationout.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            ll.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+
+
+
                 }
             }
         };
--- app/src/main/java/com/test/fcm/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/test/fcm/service/MyFirebaseMessagingService.java
@@ -73,8 +73,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             // play notification sound
             NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
             notificationUtils.playNotificationSound();
+
+
         }else{
             // If the app is in background, firebase itself handles the notification
+
+            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+            pushNotification.putExtra("message", message);
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+            // play notification sound
+            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+            notificationUtils.playNotificationSound();
         }
     }
 
@@ -99,7 +109,9 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.e(TAG, "timestamp: " + timestamp);
 
             if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+
                 // app is in foreground, broadcast the push message
+
                 Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
                 pushNotification.putExtra("message", message);
                 LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
